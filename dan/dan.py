b33462bb9eaee4bb41417df06d3414d6b370d4a2
Alzarath <alzarath@archlinux.info>
2016-04-29 01:30:06 -0400

    Added Danbooru login fields and made the search check multiple results before determining they can't view the results

--- dan/dan.py
+++ dan/dan.py
@@ -3,6 +3,15 @@ from discord.ext import commands
 from __main__ import send_cmd_help
 import aiohttp
 
+settings = {
+# Maximum image results. Capped at 100. Increasing probably isn't necessary.
+    "IMAGE_LIMIT" : 10,
+# Your Danbooru username. Sadly doesn't implement blacklists.
+    "USERNAME" : "",
+# Your Danbooru API Key. Used for basic, gold, and platinum features. Requires USERNAME.
+    "API_KEY" : ""
+}
+
 class Dan:
     def __init__(self, bot):
         self.bot = bot
@@ -15,16 +24,18 @@ class Dan:
         if len(text) > 0:
             try:
                 msg = "+".join(text)
-                search = "http://danbooru.donmai.us/posts.json?limit=1&tags=" + msg
+                search = "http://danbooru.donmai.us/posts.json?limit=" + str(settings["IMAGE_LIMIT"]) + "&tags=" + msg + check_info()
+                print(search)
                 async with aiohttp.get(search) as r:
                     result = await r.json()
                 if result != []:
                     if "success" not in result:
-                        if "file_url" in result[0]:
-                            url = "http://danbooru.donmai.us" + result[0]["file_url"]
-                            await self.bot.say(url)
-                        else:
-                            await self.bot.say("This image can not be viewed by you.")
+                        for index in range(len(result)): # Goes through each result until it finds one that works
+                            if "file_url" in result[index]:
+                                url = "http://danbooru.donmai.us" + result[index]["file_url"]
+                                await self.bot.say(url)
+                                return
+                        await self.bot.say("Cannot find an image that can be viewed by you.")
                     else:
                         await self.bot.say(result["message"] + ".")
                 else:
@@ -42,16 +53,17 @@ class Dan:
         if len(text) > 0:
             try:
                 msg = "+".join(text)
-                search = "http://danbooru.donmai.us/posts.json?limit=1&random=y&tags=" + msg
+                search = "http://danbooru.donmai.us/posts.json?limit=" + str(settings["IMAGE_LIMIT"]) + "&tags=" + msg + check_info()
                 async with aiohttp.get(search) as r:
                     result = await r.json()
                 if result != []:
                     if "success" not in result:
-                        if "file_url" in result[0]:
-                            url = "http://danbooru.donmai.us" + result[0]["file_url"]
-                            await self.bot.say(url)
-                        else:
-                            await self.bot.say("This image can not be viewed by you.")
+                        for index in range(len(result)): # Goes through each result until it finds one that works
+                            if "file_url" in result[index]:
+                                url = "http://danbooru.donmai.us" + result[index]["file_url"]
+                                await self.bot.say(url)
+                                return
+                        await self.bot.say("Cannot find an image that can not be viewed by you.")
                     else:
                         await self.bot.say(result["message"] + ".")
                 else:
@@ -62,21 +74,35 @@ class Dan:
             try:
                 msg = "+".join(text)
                 search = "http://danbooru.donmai.us/posts.json?limit=1&random=y"
+                check_info(search)
                 async with aiohttp.get(search) as r:
                     result = await r.json()
                 if result != []:
                     if "success" not in result:
-                        if "file_url" in result[0]:
-                            url = "http://danbooru.donmai.us" + result[0]["file_url"]
-                            await self.bot.say(url)
-                        else:
-                            await self.bot.say("This image can not be viewed by you.")
+                        for index in range(len(result)): # Goes through each result until it finds one that works
+                            if "file_url" in result[index]:
+                                url = "http://danbooru.donmai.us" + result[index]["file_url"]
+                                await self.bot.say(url)
+                                return
+                        await self.bot.say("Cannot find an image that can not be viewed by you.")
                     else:
                         await self.bot.say(result["message"] + ".")
                 else:
                     await self.bot.say("Your search terms gave no results.")
             except:
                 await self.bot.say("Error.")
+
+def check_info():
+    searchappend = ""
+    if settings["USERNAME"] != "":
+        searchappend += "&login=" + settings["USERNAME"]
+        if settings["API_KEY"] == "":
+            print("You must set API_KEY in cogs/dan.py for USERNAME to be relevant.")
+    if settings["API_KEY"] != "":
+        searchappend += "&api_key=" + settings["API_KEY"]
+        if settings["USERNAME"] == "":
+            print("You must set USERNAME in cogs/dan.py for API_KEY to be relevant.")
+    return searchappend
 
 def setup(bot):
     bot.add_cog(Dan(bot))
